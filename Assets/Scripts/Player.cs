d9eec812c543e549866641c5cf11bc797a78da39
YokiAot <thobutr_n@silpakorn.edu>
2026-08-01 11:20:59 +0700

    EvadingTrees

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float forcePower;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    private InputAction moveAction;
+    private Vector2 moveValue;
+
+    private int point;
+    public int Point {  get { return point; } set { point = value; } }
+
+    [SerializeField]
+    private int hp;
+    public int HP { get { return hp; } set { hp = value; } }
+    
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void MoveLeftOrRight()
+    {
+        moveValue = moveAction.ReadValue<Vector2>();
+        rb.AddForce(moveValue.x * Vector3.right * forcePower);
+    }
+}
